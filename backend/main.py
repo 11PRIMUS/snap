d1903fb51441ab859fd961088679e3d10de44074
11PRIMUS <alokpal2803@gmail.com>
2025-06-27 00:54:21 +0530

    endpoints added

--- backend/main.py
+++ backend/main.py
@@ -20,3 +20,28 @@ supabase =get_supabase()
 razorpay_client = razorpay.Client(
     auth=(os.getenv("RAZORPAY_KEY"), os.getenv("RAZORPAY_SECRET"))
 )
+
+@app.post("/upload")
+async def upload_image(file: UploadFile =File(...)):
+    file_ext = file.filename.split('.')[-1]
+    filename = f"{uuid.uuid()}.{file_ext}"
+    file_bytes = await file.read()
+
+    #to supabase
+    res = supabase.storage.from_("polariods").upload(filename, file_bytes)
+    if res.get('error'):
+        raise HTTPException(500, "try reuploading")
+    
+    public_url = supabase.storage.from_("polariods").get_public_url(filename)
+    return {"image_url": public_url, "id":filename}
+
+@app.post("/create_order")
+async def create_order(amount:float):
+    order = razorpay_client.order.create({
+        "amount": int(amount *100),
+        "currency":"INR",
+        "receipt": f"order_{uuid.uuid4()}",
+
+    })
+
+    return {"order_id":order["id"]}
