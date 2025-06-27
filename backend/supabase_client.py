515c5f38d96fbf08a24cf2785a810cd5a561e462
11PRIMUS <alokpal2803@gmail.com>
2025-06-27 11:22:30 +0530

    added env

--- backend/supabase_client.py
+++ backend/supabase_client.py
@@ -1,8 +1,11 @@
 from supabase import create_client
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_supabase():
-    supabase_url:os.getenv("SUPABASE_URL")
-    supabase_key:os.getenv("SUPABASE_KEY")
+    supabase_url = os.getenv("SUPABASE_URL")
+    supabase_key = os.getenv("SUPABASE_KEY")
 
     return create_client(supabase_url, supabase_key)
